6bb6273ee7ce8c7fd300202e909c437c5e7d5dff
Walison Thiago <walison.thiago13@gmail.com>
2025-10-24 10:25:31 -0300

    Operadores Lógicos

---
+++ JS/5_operadores-logicos.js
@@ -0,0 +1,19 @@
+const idade = 20
+const genero = "M"
+
+// && - E
+// || - Ou
+// ! - not (não)
+
+const resultado1 = idade >= 18 && genero === "M"
+const resultado2 = idade >= 18 && genero === "F"
+const resultado3 = idade >= 18 || genero === "M"
+const resultado4 = idade >= 18 || genero === "F"
+const resultado5 = idade >= 21 || genero === "F"
+
+console.log(`Você atende aos requisitos de idade ${idade} e de genero ${genero}? ${resultado1}`)
+console.log(`Você atende aos requisitos de idade ${idade} e de genero ${genero}? ${resultado2}`)
+console.log(`Você atende aos requisitos de idade ${idade} e de genero ${genero}? ${resultado3}`)
+console.log(`Você atende aos requisitos de idade ${idade} e de genero ${genero}? ${resultado4}`)
+console.log(`Você atende aos requisitos de idade ${idade} e de genero ${genero}? ${resultado5}`)
+console.log(`Você atende aos requisitos de idade ${idade} e de genero ${genero}? ${!resultado5}`)
