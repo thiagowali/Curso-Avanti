3746f165ae7871d1b0e136f6522afb60285e1257
Walison Thiago <walison.thiago13@gmail.com>
2025-10-24 08:47:56 -0300

    Operadores aritmédicos

---
+++ JS/3_operadores-aritmedicos.js
@@ -0,0 +1,20 @@
+const numero1 = 5
+const numero2 = 2
+const numero3 = 3
+const resultado1 = numero1 + numero2 //adição
+const resultado2 = numero1 - numero2 //subtração
+const resultado3 = numero1 * numero2 //multiplicação
+const resultado4 = numero1 / numero2  //divisão
+const resultado5 = numero1 % numero2  //restp
+const resultado6 = numero1 ** numero2 //exponenciação
+const resultado7 = numero1 + numero2 * numero3 //prioridades
+const resultado8 = (numero1 + numero2) / numero3 //prioridades
+
+console.log(`O resultado é ${resultado1}`)
+console.log(`O resultado é ${resultado2}`)
+console.log(`O resultado é ${resultado3}`)
+console.log(`O resultado é ${resultado4}`)
+console.log(`O resultado é ${resultado5}`)
+console.log(`O resultado é ${resultado6}`)
+console.log(`O resultado é ${resultado7}`)
+console.log(`O resultado é ${resultado8}`)
