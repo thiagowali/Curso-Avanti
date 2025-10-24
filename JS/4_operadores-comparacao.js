316466fc96922fc25ff5a605e1ede9cee16ba8d1
Walison Thiago <walison.thiago13@gmail.com>
2025-10-24 09:33:04 -0300

    Operadores de comparação

---
+++ JS/4_operadores-comparacao.js
@@ -0,0 +1,30 @@
+const numero1 = 5
+const numero2 = "5"
+const numero3 = 3
+
+// == Igualdade
+// === igualdade restrita
+// != diferença
+//!== diferença retrista
+// > maior
+// < menor 
+// >= maior igual
+// <= menor igual
+
+const resultado1 = numero1 == numero2
+const resultado2 = numero1 === numero2
+const resultado3 = numero1 != numero2
+const resultado4 = numero1 !== numero2
+const resultado5 = numero1 > numero3
+const resultado6 = numero1 < numero3
+const resultado7 = numero1 >= numero2
+const resultado8 = numero1 <= numero3
+
+console.log(`O resultado é ${resultado1}`)
+console.log(`O resultado é ${resultado2}`)
+console.log(`O resultado é ${resultado3}`)
+console.log(`O resultado é ${resultado4}`)
+console.log(`O resultado é ${resultado5}`)
+console.log(`O resultado é ${resultado6}`)
+console.log(`O resultado é ${resultado7}`)
+console.log(`O resultado é ${resultado8}`)
