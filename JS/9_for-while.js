c848d2e01cd613144a220112c96976b54029110d
Walison Thiago <walison.thiago13@gmail.com>
2025-10-27 10:40:44 -0300

    laços for e while

---
+++ JS/9_for-while.js
@@ -0,0 +1,23 @@
+
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// let i = 0;
+// while(i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+let frutas = ["banana", "maçã", "mamão"]
+
+// for(let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i])
+// }
+
+let i = 0;
+while(i < frutas.length) {
+    console.log(frutas[i])
+    i++
+}
