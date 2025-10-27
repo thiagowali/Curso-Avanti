07e2c1e7ddf53fd7647637a33599a0c05d8a5934
Walison Thiago <walison.thiago13@gmail.com>
2025-10-27 10:01:41 -0300

    estrutura if else

---
+++ JS/7_if-else.js
@@ -0,0 +1,16 @@
+const notaFinal = 7
+const frequencia = 6
+
+if (notaFinal >= 7 && frequencia >= 8) {
+    console.log("aprovado")
+} else {
+    console.log("reprovado")
+}
+
+// if (notaFinal >= 7) { 
+//     console.log("passou")
+// } else if (notaFinal >= 6) {
+//     console.log("Recuperação")
+// } else {
+//     console.log("reprovado")
+// }
