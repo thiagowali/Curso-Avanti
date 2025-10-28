c346f295dfc77a1520dd28dbb9bad2d3cfe77198
Walison Thiago <walison.thiago13@gmail.com>
2025-10-28 10:22:53 -0300

    temporizadores

---
+++ JS/16_temporarizadores.js
@@ -0,0 +1,11 @@
+// setTimeout e setInterval
+
+console.log("O resultado de 5 + 5 é ")
+
+function resultado() {
+    console.log("10")
+}
+
+//setTimeout(resultado, 5000) //calculado em milisegundos (5000 = 5 segundos)
+
+setInterval(resultado, 3000)
