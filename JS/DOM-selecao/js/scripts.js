c2478b8bb238b4b4e988e7bfc7d3b47ec75e9d65
Walison Thiago <walison.thiago13@gmail.com>
2025-10-31 09:05:07 -0300

    Seleção por ID via Javascript

---
+++ JS/DOM-selecao/js/scripts.js
@@ -0,0 +1,10 @@
+//seleção por ID
+
+const content = document.getElementById("content") //seleção por ID
+
+console.log("content", content)
+
+content.innerHTML = "<p> Parágrafo de texto</p>"
+content.style.backgroundColor = "#00f"
+content.style.fontSize = "30px"
+content.style.color = "#fff"
