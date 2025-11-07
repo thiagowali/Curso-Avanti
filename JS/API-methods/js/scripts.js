c67174ab3eda34f8f3c684d6822bf09b3b4004ba
Walison Thiago <walison.thiago13@gmail.com>
2025-11-07 09:09:55 -0300

    requisição PUT

--- JS/API-methods/js/scripts.js
+++ JS/API-methods/js/scripts.js
@@ -16,9 +16,26 @@
 
     //Requisição POST
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-  method: 'POST',
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+    //Requisição PUT
+
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'PUT',
   body: JSON.stringify({
+    id: 1,
     title: 'foo',
     body: 'bar',
     userId: 1,
