ffdf6b6d87f9d213693ebf38bd65850fe9f7ae4b
Walison Thiago <walison.thiago13@gmail.com>
2025-11-07 09:13:23 -0300

    Requisição PATCH

--- JS/API-methods/js/scripts.js
+++ JS/API-methods/js/scripts.js
@@ -32,13 +32,27 @@
 
     //Requisição PUT
 
+// fetch('https://jsonplaceholder.typicode.com/posts/1', {
+//   method: 'PUT',
+//   body: JSON.stringify({
+//     id: 1,
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+    //Requisição PATCH
+
 fetch('https://jsonplaceholder.typicode.com/posts/1', {
-  method: 'PUT',
+  method: 'PATCH',
   body: JSON.stringify({
-    id: 1,
     title: 'foo',
-    body: 'bar',
-    userId: 1,
   }),
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
