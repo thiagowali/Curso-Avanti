1d6cc72706d47bfa3ae3cdad47f6af92bd852fe3
Walison Thiago <walison.thiago13@gmail.com>
2025-11-07 09:16:25 -0300

    Requisição DELETE

--- JS/API-methods/js/scripts.js
+++ JS/API-methods/js/scripts.js
@@ -49,14 +49,20 @@
 
     //Requisição PATCH
 
+// fetch('https://jsonplaceholder.typicode.com/posts/1', {
+//   method: 'PATCH',
+//   body: JSON.stringify({
+//     title: 'foo',
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+    //Requisição DELETE
+
 fetch('https://jsonplaceholder.typicode.com/posts/1', {
-  method: 'PATCH',
-  body: JSON.stringify({
-    title: 'foo',
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+  method: 'DELETE',
+});
