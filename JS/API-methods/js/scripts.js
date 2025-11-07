a4acf6026931d28c274f682c2f1af2147b2ebaf5
Walison Thiago <walison.thiago13@gmail.com>
2025-11-07 09:00:57 -0300

    requisição POST

--- JS/API-methods/js/scripts.js
+++ JS/API-methods/js/scripts.js
@@ -1,13 +1,31 @@
-fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((posts) => {
-        console.log(posts);
-        const container = document.querySelector("#container")
-        posts.map(post => {
-            console.log(post.title)
-            const h2 = document.createElement("h2")
-            h2.textContent = post.title
-            container.appendChild(h2)
-        })
+    //Requisição GET
 
-    })
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((response) => response.json())
+//     .then((posts) => {
+//         console.log(posts);
+//         const container = document.querySelector("#container")
+//         posts.map(post => {
+//             console.log(post.title)
+//             const h2 = document.createElement("h2")
+//             h2.textContent = post.title
+//             container.appendChild(h2)
+//         })
+
+//     })
+
+    //Requisição POST
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
